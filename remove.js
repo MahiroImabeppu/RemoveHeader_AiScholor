2311df229c39f1eae2e4c3ca89f7735a494a5ba2
mahiro <mahiro.1192@gmail.com>
2020-07-20 15:50:18 +0900

    いらない部分を消しました

--- remove.js
+++ remove.js
@@ -1,20 +1,4 @@
-﻿var cmt;
-
-//テキスト表示部分
-var showTxt = document.createElement("div"); // p要素作成
-var newContent = document.createTextNode("コメント"); // テキストノードを作成
-showTxt.appendChild(newContent); // p要素にテキストノードを追加
-showTxt.setAttribute("id","flowText"); // p要素にidを設定
-showTxt.style.position = "absolute";
-showTxt.style.zIndex = 999;
-showTxt.style.color = "white";
-showTxt.style.fontSize = "80px";
-showTxt.style.top = "100px";
-var parent = document.getElementsByTagName("body")[0];
-parent.insertBefore(showTxt, parent.firstChild);
-
-
-function remove_stikyHeader(){
+﻿function remove_stikyHeader(){
     var header =  document.getElementsByTagName("header");
 
     header[0].style.position = 'relative';
